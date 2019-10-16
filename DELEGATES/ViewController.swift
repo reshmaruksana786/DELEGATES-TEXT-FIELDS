bd9922fb2bbcee24ad16362c18ed79e93226652e
Syed.Reshma Ruksana <reshmasmile468@achiever-8.local>
2019-10-16 12:49:03 +0530

    Initial Commit

---
+++ DELEGATES/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DELEGATES
+//
+//  Created by Syed.Reshma Ruksana on 16/10/19.
+//  Copyright © 2019 Syed.Reshma Ruksana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
